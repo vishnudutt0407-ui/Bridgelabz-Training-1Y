3fd9464b43fbf645d4ede6b5c74a2d82815820c6
vihnudutto4o7-ui <vishnudutto4o7@gmail.com>
2026-01-28 15:11:02 +0530

    For each example

---
+++ Basic-programs/Foreach.java
@@ -0,0 +1,10 @@
+class Foreach{
+public static void main(String[] args){
+System.out.println("for each loop example");
+int arr[]={10,20,30,40};
+
+for(int num: arr){
+System.out.println(num);
+}
+}
+}
